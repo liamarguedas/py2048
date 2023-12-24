b5d96e138ed0ec8ab54f11e0a604a706420849ca
liano <iliamftw2013@gmail.com>
2023-12-24 19:37:55 -0300

    Add (GameClass)

--- client.py
+++ client.py
@@ -1,54 +1,14 @@
+from objects import GameClass
 import sys, pygame
 
-pygame.init()
+game = GameClass(window_name="py2048")
 
-# Window settings
-WIDTH = 600
-HEIGHT = 600
-SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("py2048")
-
-# Game items and colors
-BACKGROUND = (250, 248, 239)
-
-TABLE_COLOR = (119, 110, 101)
-TABLE_SIZE = 500
-TABLE_X = (WIDTH - TABLE_SIZE) // 2
-TABLE_Y = (HEIGHT - TABLE_SIZE) // 2
-
-# TILES
-DEFAULT_TILE_COLOR = (205,193,180)
-
-TILE_SIZE = 200
-
-TILE_WIDTH = TILE_SIZE // 2
-TILE_HEIGHT = TILE_SIZE // 2
-
-TILE_SPACING = TILE_SIZE // 10
-
-TITLE_STARTING_X = TILE_SPACING + TABLE_X
-TITLE_STARTING_Y = TILE_SPACING + TABLE_Y
-
-while True:
-    # Quit while loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-
+while game.GAMESTATUS():
     # Drawing the main window
-    SCREEN.fill(BACKGROUND)
+    game.SET_SCREEN()
 
     # 2048 TABLE
-    pygame.draw.rect(SCREEN, TABLE_COLOR, (TABLE_X, TABLE_Y, TABLE_SIZE, TABLE_SIZE))
-
-    # TILES
-    pygame.draw.rect(SCREEN, DEFAULT_TILE_COLOR, (TITLE_STARTING_X, TITLE_STARTING_Y, TILE_WIDTH, TILE_HEIGHT))
-    pygame.draw.rect(SCREEN, DEFAULT_TILE_COLOR, ((TITLE_STARTING_X + TILE_WIDTH + TILE_SPACING), TITLE_STARTING_Y, TILE_WIDTH, TILE_HEIGHT))
-    pygame.draw.rect(SCREEN, DEFAULT_TILE_COLOR, (TITLE_STARTING_X + (TILE_WIDTH * 2) + (TILE_SPACING * 2), TITLE_STARTING_Y, TILE_WIDTH, TILE_HEIGHT))
-    pygame.draw.rect(SCREEN, DEFAULT_TILE_COLOR, (TITLE_STARTING_X + (TILE_WIDTH * 3) + (TILE_SPACING * 3), TITLE_STARTING_Y, TILE_WIDTH, TILE_HEIGHT))
+    game.GAMETABLE()
 
     # Update the display
-    pygame.display.flip()
-
-    # FPS Controller
-    pygame.time.Clock().tick(60)  # FPS
+    game.UPDATE_SCREEN(FPS=60)
--- objects.py
+++ objects.py
@@ -1,2 +1,49 @@
-import pygame
+import pygame, sys
 
+
+class GameClass:
+    def __init__(self, window_name) -> None:
+        pygame.init()
+        pygame.display.set_caption(window_name)
+
+        # Window settings
+        self.WIDTH = 600
+        self.HEIGHT = 600
+        self.SCREEN = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        self.BACKGROUND = (250, 248, 239)
+
+        # Game items and colors
+        self.TABLE_COLOR = (119, 110, 101)
+        self.TABLE_SIZE = 500
+        self.TABLE_X = (self.WIDTH - self.TABLE_SIZE) // 2
+        self.TABLE_Y = (self.HEIGHT - self.TABLE_SIZE) // 2
+
+        # TILES
+        self.DEFAULT_TILE = (205, 193, 180)
+        self.TILE_SIZE = 200
+        self.TILE_WIDTH = self.TILE_SIZE // 2
+        self.TILE_HEIGHT = self.TILE_SIZE // 2
+        self.TILE_SPACING = self.TILE_SIZE // 10
+
+    def GAMESTATUS(self) -> bool:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+        return True
+
+    def SET_SCREEN(self):
+        self.SCREEN.fill(self.BACKGROUND)
+
+    def GAMETABLE(self):
+        pygame.draw.rect(
+            self.SCREEN,
+            self.TABLE_COLOR,
+            (self.TABLE_X, self.TABLE_Y, self.TABLE_SIZE, self.TABLE_SIZE),
+        )
+
+    def TILE(self):
+        pass
+
+    def UPDATE_SCREEN(self, FPS=60):
+        pygame.display.flip()
+        pygame.time.Clock().tick(FPS)
